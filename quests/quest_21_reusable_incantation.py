9a96e038dba66dd124d6ffb9a60fc88e2963158a
hidriss-collab <h.idriss@alustudent.com>
2026-06-29 05:50:22 -0700

    Complete Quest 21

---
+++ quests/quest_21_reusable_incantation.py
@@ -0,0 +1,8 @@
+# This function prints a welcome message.
+def greet_adventurer():
+    print("Welcome, brave adventurer!")
+
+# Call the function three times.
+greet_adventurer()
+greet_adventurer()
+greet_adventurer()
