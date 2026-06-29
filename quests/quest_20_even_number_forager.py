3218ab4e3d15822ff274d01f139ef2669fd5e34b
k-gloire <g.umuhendak@alustudent.com>
2026-06-29 14:13:06 +0200

    Update quest_20_even_number_forager.py

--- quests/quest_20_even_number_forager.py
+++ quests/quest_20_even_number_forager.py
@@ -1,4 +1,6 @@
 # The concept here is using the if insde a for loop to filter numbers 
-for i in range(1, 21):
+#here we loop through numbers 1 to 20
+for i in range(1, 21): 
+# if i % 2 equals to 0, the number has no remainder so it is even
     if i % 2 == 0:
-        print(i)
+        print(i) # print only even numbers
