ce27be2bd62c274285bba5658d475a8db3affdc8
k-gloire <g.umuhendak@alustudent.com>
2026-06-29 11:22:06 +0200

    Add Level 4 quests 16-20

---
+++ quests/quest_18_loop_of_riddles.py
@@ -0,0 +1,9 @@
+secret_number = 7
+
+guess = int(input("Guess the secret number: "))
+
+while guess != ____:
+    print("Wrong! Try again.")
+    guess = int(input("Guess again: "))
+
+print("____ you got it!")
