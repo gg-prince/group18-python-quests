ad577947ba5ba6c18ccfc258f14c8e6571a1e3fb
k-gloire <g.umuhendak@alustudent.com>
2026-06-29 13:37:51 +0200

    Update quest_17_endless_staircase.py

--- quests/quest_17_endless_staircase.py
+++ quests/quest_17_endless_staircase.py
@@ -1,4 +1,6 @@
+#The concept here is the while loop where the code repeats itself as long as a condition is true 
 counter = 0
+#keep looping as long as counter is less than 5
 while counter < 5:
     print(counter)
-    counter += 1
+    counter += 1 #Here we increase the counter by 1 to avoid and infinite loop
