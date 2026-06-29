a0b216a1043c9d8652b2562f5271f0111262c4ee
k-gloire <g.umuhendak@alustudent.com>
2026-06-29 14:09:10 +0200

    Update quest_19_countdown.py

--- quests/quest_19_countdown.py
+++ quests/quest_19_countdown.py
@@ -1,4 +1,5 @@
+#The range here has three arguments -start,stop,step
+#range(10,0,-1) starts at 10, stops before 0, steps by -1 each time
 for i in range(10, 0, -1):
-    print(i)
-
-print("Blastoff!")
+    print(i) #Here we print each number in the countdown  
+print("Blastoff!") # we print the final message after the loop ends
